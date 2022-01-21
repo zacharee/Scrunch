42534568123b0fe5b6f4311cde247f2db6f3f3ee
Zachary Wander <zachary.wander@gmail.com>
2022-01-21 17:48:27 -0500

    Work on migrating to DataStore

--- app/src/main/java/com/denytheflowerpot/scrunch/managers/SettingsManager.kt
+++ app/src/main/java/com/denytheflowerpot/scrunch/managers/SettingsManager.kt
@@ -2,25 +2,49 @@ package com.denytheflowerpot.scrunch.managers
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.denytheflowerpot.scrunch.ScrunchApplication
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class SettingsManager(private val context: Context) {
     private class Keys {
         companion object {
-            public val UnfoldSoundURL = "unfoldSoundURL"
+            public val UnfoldSoundURL = stringPreferencesKey("unfoldSoundURL")
             public val FoldSoundURL = "foldSoundURL"
             public val ServiceStarted = "serviceStarted"
             public val Volume = "volume"
         }
     }
-    val prefs: SharedPreferences by lazy {
-        context.getSharedPreferences("${context.packageName}.PREFS", Context.MODE_PRIVATE)
-    }
-    var unfoldSoundURL: String
-        get() = prefs.getString(Keys.UnfoldSoundURL, "") ?: ""
-        set(value) {
-            prefs.edit().putString(Keys.UnfoldSoundURL, value).apply()
+    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+        name = "settings",
+        produceMigrations = {
+            listOf(
+                SharedPreferencesMigration(it, "${context.packageName}.PREFS")
+            )
         }
+    )
+
+    val unfoldSoundURL: Flow<String>
+        get() = context.dataStore.data
+            .map { prefs ->
+                prefs[Keys.UnfoldSoundURL] ?: ""
+            }
+
+    suspend fun updateUnfoldSoundURL(url: String) {
+        context.dataStore.edit { prefs ->
+            prefs[Keys.UnfoldSoundURL] = url
+        }
+    }
+
+    val foldSoundURL: Flow<String>
+
 
     var foldSoundURL: String
         get() = prefs.getString(Keys.FoldSoundURL, "") ?: ""
